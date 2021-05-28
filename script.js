64466ed7793e58c68a969d3ef19019ff95b1fbe4
alisonarmstrong <alarmstrong15@gmail.com>
2021-05-28 11:11:48 -0600

    pages working now

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ var choice2 = document.getElementById("choice2");
 var choice3 = document.getElementById("choice3");
 var choice4 = document.getElementById("choice4");
 var correct = document.getElementById("correct");
-var answer = document.getElementById("answer");
+var answerResponse = document.getElementById("answerResponse");
 var finalScoreIs = document.getElementById("finalScoreIs");
 var quizQuestionsPage = document.getElementById("quizQuestionsPage");
 var questionButton = document.querySelector(".questionButton");
@@ -23,43 +23,45 @@ var timer = document.getElementById("timer");
 var highScoreArray = []
 var score = 0;
 var questionIndex = 0;
+var timerInterval;
+var highScoreList = document.getElementById("highScoreList");
 
-var questions = [
+var quizQuestions = [
   {
-    "question": "Commonly used Data Types do NOT Include:",
-    "one": "1. strings",
-    "two": "2. booleans",
-    "three": "3. alerts",
-    "four": "4. numbers",
-    "correct": "3. alerts",
+    question: "Commonly used Data Types do NOT Include:",
+    one: "1. strings",
+    two: "2. booleans",
+    three: "3. alerts",
+    four: "4. numbers",
+    correct: "3. alerts",
   }, {
-    "question": "The condition in an if / else statement is enclosed within ________.",
-    "choice1": "1. quotes",
-    "two": "2. curly brackets",
-    "three": "3. parenthesis",
-    "four": "4. square brackets",
-    "correct": "3. parenthesis",
+    question: "The condition in an if / else statement is enclosed within ________.",
+    one: "1. quotes",
+    two: "2. curly brackets",
+    three: "3. parenthesis",
+    four: "4. square brackets",
+    correct: "3. parenthesis",
   }, {
-    "question": "Arrays in JavaScript can be used to store ________.",
-    "choice1": "1. numbers and strings",
-    "choice2": "2. other arrays",
-    "choice3": "3. booleans",
-    "choice4": "4. all of the above",
-    "correct": "4. all of the above",
+    question: "Arrays in JavaScript can be used to store ________.",
+    one: "1. numbers and strings",
+    two: "2. other arrays",
+    three: "3. booleans",
+    four: "4. all of the above",
+    correct: "4. all of the above",
   }, {
-    "question": "String values must be enclosed within ________ when being assigned to variables",
-    "choice1": "1. commas",
-    "choice2": "2. curly brackets",
-    "choice3": "3. quotes",
-    "choice4": "4. parenthesis",
-    "correct": "3. quotes",
+    question: "String values must be enclosed within ________ when being assigned to variables",
+    one: "1. commas",
+    two: "2. curly brackets",
+    three: "3. quotes",
+    four: "4. parenthesis",
+    correct: "3. quotes",
   }, {
-    "question": "A very useful tool used for developing and debugging for printing content to the debugger is:",
-    "choice1": "1. JavaScript",
-    "choice2": "2. terminal / bash",
-    "choice3": "3. for loops",
-    "choice4": "4. console.log",
-    "correct": "4. console.log",
+    question: "A very useful tool used for developing and debugging for printing content to the debugger is:",
+    one: "1. JavaScript",
+    two: "2. terminal / bash",
+    three: "3. for loops",
+    four: "4. console.log",
+    correct: "4. console.log",
   },
 ]
 
@@ -68,6 +70,7 @@ function codeQuizChallenge() {
   header.style.display = "block";
   quizQuestionsPage.style.display = "none";
   finalScorePage.style.display = "none";
+  highScoreButtons.style.display = "none";
 
   var startScore = 0;
   timer.textContent = "Time: " + startScore;
@@ -81,10 +84,10 @@ function startQuiz() {
 
   secondsLeft = 80;
 
-  var timerInterval = setInterval(function () {
+  timerInterval = setInterval(function () {
     secondsLeft--;
     timer.textContent = "Time: " + secondsLeft;
-    if (secondsLeft === 0 || quizQuestionsPage.length === questionIndex) {
+    if (secondsLeft === 0 || quizQuestions.length === questionIndex) {
       clearInterval(timerInterval);
       showFinalScore();
     }
@@ -94,14 +97,14 @@ function startQuiz() {
 function showQuestions() {
   var q = quizQuestions[questionIndex];
 
-  quizQuestionHeader.innerHTML = q.quizQuestionHeader;
+  quizQuestionHeader.innerHTML = q.question;
   choice1.innerHTML = q.one;
   choice1.setAttribute("data-answer", q.one);
-  choice2.innerHTML = q.one;
+  choice2.innerHTML = q.two;
   choice2.setAttribute("data-answer", q.two);
-  choice3.innerHTML = q.one;
+  choice3.innerHTML = q.three;
   choice3.setAttribute("data-answer", q.three);
-  choice4.innerHTML = q.one;
+  choice4.innerHTML = q.four;
   choice4.setAttribute("data-answer", q.four);
 }
 
@@ -129,7 +132,7 @@ function checkAnswer(event) {
     correctAnswer = answer;
   }
   if (answer === correctAnswer) {
-    answer.Response.textContent = "Correct";
+    answerResponse.textContent = "Correct";
   } else {
     answerResponse.textContent = "Incorrect";
     secondsLeft -= 10
@@ -146,6 +149,7 @@ function checkAnswer(event) {
 }
 
 function showFinalScore() {
+  clearInterval(timerInterval);
   quizQuestionsPage.style.display = "none";
   highScoreButtons.style.display = "none";
   finalScorePage.style.display = "block";
@@ -161,12 +165,14 @@ function showFinalScore() {
 
 function showHighScores() {
   quizQuestionsPage.style.display = "none";
-  highScoreButtons.style.display = "none";
+  // highScoreButtons.style.display = "none";
   finalScorePage.style.display = "none";
   initials.style.display = "none";
   initialButton.style.display = 'none';
   initialInput.style.display = "none";
+  console.log("before high scores")
   highScoreButtons.style.display = "block";
+  console.log("after high scores")
 
   var getInitials = document.getElementById("initialInput").value;
   var highScoreArray = JSON.parse(localStorage.getItem("highScore")) || [];
@@ -183,9 +189,10 @@ submitButton.addEventListener("click", function() {
 initialButton.addEventListener("click", function() {
   showHighScores();
 })
-// clearHighScore.addEventListener("click", function() {
-//   localStorage.clear();
-// })
+clearHighScore.addEventListener("click", function() {
+  localStorage.clear();
+  highScoreList.innerHTML="";
+})
 goBack.addEventListener("click", function() {
   $("#highScoreList").empty()
   $("#initialInput").val("")
